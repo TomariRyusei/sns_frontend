d119c4135dcc20309fc942557426ddc5b870bc97
TomariRyusei <t.ryu6635@gmail.com>
2022-06-14 11:25:53 +0900

    Update auth page

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,9 +1,47 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "./Auth.css";
-import SigUp from "./SignUp";
-import LogIn from "./Login";
 const Auth = () => {
+  // フォームデータの初期値
+  const initialState = {
+    firstname: "",
+    lastname: "",
+    username: "",
+    password: "",
+    confirmpass: "",
+  };
+
+  // ログイン/登録の表示切り替え
+  const [isSignUp, setIsSignUp] = useState(true);
+
+  // フォームデータ
+  const [data, setData] = useState(initialState);
+
+  // 登録時パスワード一致確認
+  const [confirmPass, setConfirmPass] = useState(true);
+
+  const handleFormDataChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  // フォームサブミット
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isSignUp) {
+      if (data.password !== data.confirmpass) {
+        setConfirmPass(false);
+      } else {
+        setConfirmPass(true);
+      }
+    } else {
+    }
+  };
+
+  //　フォーム情報を初期化
+  const resetForm = () => {
+    setData(initialState);
+    setConfirmPass(true);
+  };
+
   return (
     <div className="Auth">
       <div className="AuthPageLeft">
@@ -13,8 +51,94 @@ const Auth = () => {
           <h6>Twitterを模したSNSサービスです。名前はないです。</h6>
         </div>
       </div>
-      {/* <SignUp /> */}
-      <LogIn />
+      <div className="AuthPageRight">
+        <form className="InfoForm AuthForm" onSubmit={handleSubmit}>
+          <h3>{isSignUp ? "登録" : "ログイン"}</h3>
+          {isSignUp && (
+            <div>
+              <input
+                type="text"
+                placeholder="名"
+                className="InfoInput"
+                name="firstname"
+                onChange={handleFormDataChange}
+                value={data.firstname}
+              />
+              <input
+                type="text"
+                placeholder="姓"
+                className="InfoInput"
+                name="lastname"
+                onChange={handleFormDataChange}
+                value={data.lastname}
+              />
+            </div>
+          )}
+
+          <div>
+            <input
+              type="text"
+              className="InfoInput"
+              name="username"
+              placeholder="ユーザー名"
+              onChange={handleFormDataChange}
+              value={data.username}
+            />
+          </div>
+
+          <div>
+            <input
+              type="password"
+              className="InfoInput"
+              name="password"
+              placeholder="パスワード"
+              onChange={handleFormDataChange}
+              value={data.password}
+            />
+            {isSignUp && (
+              <input
+                type="password"
+                className="InfoInput"
+                name="confirmpass"
+                placeholder="パスワード(確認用)"
+                onChange={handleFormDataChange}
+                value={data.confirmpass}
+              />
+            )}
+          </div>
+          <span
+            style={{
+              color: "red",
+              fontSize: "12px",
+              marginRight: "5px",
+              display: confirmPass ? "none" : "block",
+            }}
+          >
+            ※確認用パスワードが一致しません。
+          </span>
+
+          <div>
+            <span
+              style={{
+                fontSize: "12px",
+                cursor: "pointer",
+                textDecoration: "underline",
+              }}
+              onClick={() => {
+                resetForm();
+                setIsSignUp((prev) => !prev);
+              }}
+            >
+              {isSignUp
+                ? "すでにアカウントをお持ちのようです。 ログインしてください。"
+                : "アカウントをお持ちでないようです。登録をお願いします。"}
+            </span>
+          </div>
+          <button className="button InfoButton" type="submit">
+            {isSignUp ? "登録" : "ログイン"}
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
